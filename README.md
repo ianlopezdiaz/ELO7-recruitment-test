e6b58adf09364dc5cb9947a37c55615888d2b3ac
ianlopezdiaz <ianlopezdiaz@gmail.com>
2021-04-30 10:09:28 -0300

    additional changes

---
+++ README.md
@@ -0,0 +1,22 @@
+# Teste Data Science Elo7
+
+A versão completa deste trabalho eventualmente terá as seguintes partes:
+
+1. **Análise exploratória**
+2. **Sistema de classificação de produtos**
+3. **Sistema de termos de busca**
+4. **Avaliação dos sistemas de classificação**
+5. **Colaboração entre os sistemas**
+6. **Entrega dos sistemas**
+
+Por enquanto há apenas a **Análise exploratória** neste [jupyter notebook](01_analise_exploratoria.ipynb).
+Futuramente haverá algo da parte 2 deste teste [aqui](02_classificacao_de_produto.ipynb)
+e das demais etapas em outros notebooks que ainda não foram criados. O script [teste.py](teste.py)
+tem apenas um esqueleto para representar o que ele fará no futuro.
+
+
+Instale as bibliotecas necessárias:
+
+```shell
+pip install -r requirements.txt
+```
---
+++ teste.py
@@ -0,0 +1,44 @@
+"""
+Ainda não há nada implementado aqui. Apenas um esqueleto de script.
+"""
+
+
+import sys
+
+def print_help():
+    help_string = """
+    Usage:
+        python teste.py --category "{'feature_1':<input_da_feature_1>,'feature_2':<input_da_feature_2>,...}"
+        python teste.py --intent "<termo_de_busca_do_usuário>"
+        python teste.py --help
+    """
+    print(helt_string)
+
+def category(features_dict):
+    return features_dict
+
+def intent(user_query):
+    return user_query
+
+def recommendation(user_query):
+    return user_query
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        if sys.argv[1] in ('--category', '--intent', '--recommendation'):
+            if sys.argv[1] == '--category':
+                features_dict = eval(sys.argv[2])
+                if type(features_dict) == dict:
+                    print(category(features_dict))
+                else:
+                    print_help()
+            elif sys.argv[1] == '--intent':
+                print(intent(sys.argv[2]))
+            else:
+                print(recommendation(sys.argv[2]))
+        else:
+            print_help()
+    else:
+        print_help()
+
